b990f509758b07abe26e4540440f7593f5d470f2
Simon <simonsch.tz@gmail.com>
2020-12-17 20:38:56 +0100

    Add reg-components to git-stack

---
+++ src/components/registration-components/RegStep1.js
@@ -0,0 +1,33 @@
+import React, {useContext} from 'react';
+import { Context } from '../../Context';
+
+export default function RegStepOne() {
+    const {fullName, setFullName, step} = useContext(Context);
+    const side = () => {
+        if (step === 1) {
+            return "center"
+        } else {
+            return "to-left"
+        }
+    }
+    return (
+        <div className={`registration-step ${side()}`}>
+            <h2>Gib deinen Namen ein</h2>
+            <p>
+            Wichtig ist, dass du deinen richtigen Namen eingibst, da wir nur so deine Qualifikation zuordnen können.
+            </p>
+            <p>
+            <span>Hinweis:</span> Für eine vollständige Registrierung muss ein Nachweis als Berufs-KFZler erbracht werden.
+            </p>
+            <label htmlFor="fullName">
+                <input 
+                required 
+                type="fullName" 
+                id="fullName" 
+                placeholder="E-Mail Adresse oder Benutzername" 
+                value={fullName} 
+                onChange={(e) => setFullName(e.target.value)} />
+            </label>
+        </div>
+    )
+}
---
+++ src/components/registration-components/RegStep2.js
@@ -0,0 +1,28 @@
+import React, {useContext} from 'react';
+import { Context } from '../../Context';
+
+export default function RegStepOne() {
+    const {email, setEmail, step} = useContext(Context);
+    const side = () => {
+        if (step === 2) return "center"
+        if (step < 2 ) return "to-right"
+        if (step > 2 ) return "to-left"
+    }
+    return (
+        <div className={`${side()}`}>
+            <h2>Gib deinen E-Maio Adresse ein</h2>
+            <label htmlFor="email">
+                <input 
+                required 
+                type="email" 
+                id="email" 
+                placeholder="E-Mail Adresse oder Benutzername" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} />
+            </label>
+            <p>
+            Mit dem Fortfahren der Registrierung bestätige ich die Hinweise zum Datenschutz sowie die geltenden Nutzungsbedingungen gelesen und akzeptiert zu haben.
+            </p>
+        </div>
+    )
+}
