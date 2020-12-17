099747572f23512f2266d0c6bf0437cee8679797
Simon <simonsch.tz@gmail.com>
2020-12-17 11:19:38 +0100

    Initialize navigation infrastructure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.scss';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Landing from './Landing';
+import Registration from './Registration';
 
 function App() {
   return (
     <div className="App">
-    <Landing />
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/" render={() => <Landing/>} />
+      <Route exact path="/registration" render={() => <Registration/>} />
+          
+    </Switch>
+
+    </BrowserRouter>
     </div>
   );
 }
